dab9bd598ec1993b709c0f130d03bef2c619c2d2
Hema Hamsaveni M <hemahamsaveni08@gmail.com>
2026-04-13 15:55:09 +0530

    SampleCommit

--- src/main/java/com/sprint/BookPartnerApplication/BookPartnerApplication.java
+++ src/main/java/com/sprint/BookPartnerApplication/BookPartnerApplication.java
@@ -1,7 +1,9 @@
 package com.sprint.BookPartnerApplication;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookPartnerApplication {
@@ -9,5 +11,12 @@ public class BookPartnerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookPartnerApplication.class, args);
 	}
+	
+	@Bean
+    public CommandLineRunner run() {
+        return args -> {
+            System.out.println("Hello from HemaHamsaveni's branch! Spring Boot started successfully.");
+        };
+    }
 
 }
