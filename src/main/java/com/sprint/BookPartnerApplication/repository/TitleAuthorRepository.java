7f55ee3cdee6ab6d383adc60057ff6844d6e724b
Hema Hamsaveni M <hemahamsaveni08@gmail.com>
2026-04-15 10:40:44 +0530

    Ignore Commit

--- src/main/java/com/sprint/BookPartnerApplication/repository/TitleAuthorRepository.java
+++ src/main/java/com/sprint/BookPartnerApplication/repository/TitleAuthorRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TitleAuthorRepository extends JpaRepository<TitleAuthor, TitleAuthorId> {
+public interface TitleAuthorRepository extends JpaRepository<TitleAuthor, TitleAuthorId> 
+{
+	
 }
 
