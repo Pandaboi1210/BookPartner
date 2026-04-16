ec5ff3c25b980496dffb7bcb7741cba4282a730e
HARINI <rtsharini@gmail.com>
2026-04-16 11:02:37 +0530

    Updated Controller, Service, Repository

--- src/main/java/com/sprint/BookPartnerApplication/repository/DiscountRepository.java
+++ src/main/java/com/sprint/BookPartnerApplication/repository/DiscountRepository.java
@@ -1,9 +1,14 @@
 package com.sprint.BookPartnerApplication.repository;
 
 import com.sprint.BookPartnerApplication.entity.Discounts;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DiscountRepository extends JpaRepository<Discounts, Integer> {
+
+	List<Discounts> findByStore_StorId(String storId);
 }
