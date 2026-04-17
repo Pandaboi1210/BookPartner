a4c11dce9c07278eeec6a75b65bddbe1a10e6c15
Hema Hamsaveni M <hemahamsaveni08@gmail.com>
2026-04-17 08:37:54 +0530

    Custom Queries for Title, TitleAuthor

--- src/main/java/com/sprint/BookPartnerApplication/repository/TitleAuthorRepository.java
+++ src/main/java/com/sprint/BookPartnerApplication/repository/TitleAuthorRepository.java
@@ -6,11 +6,14 @@ import com.sprint.BookPartnerApplication.entity.TitleAuthorId;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query; 
+import org.springframework.data.repository.query.Param; 
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TitleAuthorRepository extends JpaRepository<TitleAuthor, TitleAuthorId> 
-{
-	List<TitleAuthor> findByTitle_TitleId(String titleId);
-}
+public interface TitleAuthorRepository extends JpaRepository<TitleAuthor, TitleAuthorId> {
 
+    @Query("SELECT ta FROM TitleAuthor ta WHERE ta.titleId = :titleId")
+    List<TitleAuthor> findByTitle_TitleId(@Param("titleId") String titleId);
+    
+}
